83e11de7236f3678aeecc9f4b1cc3b8c77606b58
harshmaan <32389454+harshmaan@users.noreply.github.com>
2023-05-24 14:54:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as streamlit
+from utils import PrepProcessor, columns
+
+import numpy as np 
+import pandas as pd
+import joblib 
+
+model = joblib.load('xgbpipe.joblib')
+
+st.title('Business Intelligence SQL Migration Automation Tool :report:')
+st.text_input('Enter sql file name', "Please enter your file name in format: FILE_NAME_DATE")
+st.select_slider('Choose base sql class',['Teradata','Oracle','DVT'])
+st.select_slider('Choose target sql class',['Snowflake','Oracle'])
+st.text_input('Enter sql file', "Please paste your base sql here")
+
+
+def convert():
+	st.success('Conversion successful :thumbsup:')
+
+st.button('Convert', on_clock=convert)
